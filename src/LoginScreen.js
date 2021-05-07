03462a73457af65f6b9a0516e9cfb8d5146ea4e0
Rosadiorio96 <rosa.diorio96@gmail.com>
2021-05-07 17:45:47 +0200

    Commit nuovo

--- src/LoginScreen.js
+++ src/LoginScreen.js
@@ -57,7 +57,7 @@ const api_login_call= async (username, password, navigation)=>{
       });
 
     }catch(e){
-      console.log("erroreeee");
+      console.log("ERRORE");
       console.log(e);
     }
   
